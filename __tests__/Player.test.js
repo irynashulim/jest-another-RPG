b77b0ca8b243aac87c2b4600c7c6291bc3b9c084
Iryna <iryna.shulim@gmail.com>
2021-07-06 14:11:35 -0500

    enemy object created and tested

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,6 +2,7 @@ const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
 
+console.log(new Potion());
 test('creates a player object', () => {
     const player = new Player('Dave');
   
@@ -16,7 +17,7 @@ test('creates a player object', () => {
 test("gets player's stats as an object", () => {
     const player = new Player('Dave');
   
-    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('potions'); 
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
